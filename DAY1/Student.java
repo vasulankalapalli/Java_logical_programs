659f8f9288d932c1c32dfab51c78b6196da6bfc0
vasulankalapalli <vasulankalapalli27@gmail.com>
2024-11-07 20:35:41 +0530

    Add files via upload

---
+++ DAY1/Student.java
@@ -0,0 +1,14 @@
+	class Student
+{
+public static void main (String args[])
+{
+int stid=101;
+String stname="Vasu";
+String stsec="MCA-B";
+double stcgpa=8.33;
+System.out.println("Student Roll Number is :"+stid);
+System.out.println("Student Name is :"+stname);
+System.out.println("Student Group  is :"+stsec);
+System.out.println("Student Overall Marks in CGPA is :"+stcgpa);
+}
+}
